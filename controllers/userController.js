44af7d596c3a4ac3ef800b8b9598d1ec93c86e98
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-04-26 12:03:33 +0200

    add user login route and generate jwt when the user is successfully signing in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,85 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
-
+const generateToken= require('../utils/generateToken')
 
 //@ route: POST/  /register
-//@ description: add new user 
+//@ description: add new user
 //@ access: public
-exports.postRegisterUser = asyncHandler( async(req, res, next)=>{
-    const {username, email, password, confirmedPassword} = req.body;
-    if(!email ){
-        res.status(400)
-        throw new Error('email is required');
-    }
-    
-    if(!password  || password.length < 6){
-        res.status(400)
-        throw new Error('password is required and have to be at least 6 characters long');
-    }
-    
-    if(password !== confirmedPassword ){
-        res.status(400)
-        throw new Error('password dose not match');
-    }
-    
-    if(!username){
-        username = email;
-    }
-    
-    const userExist = await User.findOne({email:email})
-    
-    if(userExist){
-        res.status(400);
-        throw new Error('user already exists')
-    }
-    
-    const hashedPassword = (await bcrypt.hash(password, 12)).toString()
-    
-    const user = await User.create({username:username, email:email, password:hashedPassword })
-    
-    if(user){
+exports.postRegisterUser = asyncHandler(async (req, res, next) => {
+  const { username, email, password, confirmedPassword } = req.body;
+  if (!email) {
+    res.status(400);
+    throw new Error("email is required");
+  }
+
+  if (!password || password.length < 6) {
+    res.status(400);
+    throw new Error("password is required and have to be at least 6 characters long");
+}
+
+  if (password !== confirmedPassword) {
+    res.status(400);
+    throw new Error("password dose not match");
+  }
+
+  if (!username) {
+    username = email;
+  }
+
+  const userExist = await User.findOne({ email: email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("user already exists");
+  }
+
+  const hashedPassword = (await bcrypt.hash(password, 12)).toString();
+
+  const user = await User.create({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+
+  if (user) {
     res.status(201).json({
-        username: user.username,
-    })
-    }else{
-        res.status(400);
-        throw new Error('invalid user data, something went wrong')
-    }
-    
-    })
+      username: user.username,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data, something went wrong");
+  }
+});
+
+
+
+//@ route: POST/  /login
+//@ description: Auth user & get JWT
+//@ access: public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if(!user){
+      res.status(404);
+      throw new Error('no account is attached with this email');
+  }
+
+  //check if the provided password match the password in the database
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (user && passwordMatch) {
+    res.json({
+      _id: user._id,
+      name: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("invalid email or password");
+  }
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
  
-
+router.post('/login',userController.login);
 router.post('/register', userController.postRegisterUser);
 
 
