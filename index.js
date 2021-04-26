1286363a214c3b1895b4d6c655b337d5dbd86ce8
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-04-26 12:34:28 +0200

    setup multer to handle image upload

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ const cors =require('cors');
 require('dotenv').config()
 const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 const userRoutes = require('./routes/userRoutes');
-
+const multer = require('multer');
 const app = express();
 app.use(cors())
 app.use(express.json());
 
 
 
+app.use('/images',express.static('images'))
+
+////configure storage for uploaded images /multer
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'images')
+    },
+    filename: (req, file, cb)=>{
+        cb(null, req.body.portraitStyle + '-' + file.originalname)
+    }
+});
+
+/// check if uploaded files are images before saving 
+const fileFilter = (req, file, cb)=>{
+    if(file.mimetype === 'image/png'  || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+        cb(null, true);
+    }else{
+        cb(null, false);
+    }}
+
+app.use(multer({storage:fileStorage, fileFilter:fileFilter }).single('image'))
 
 
 app.use(userRoutes);
